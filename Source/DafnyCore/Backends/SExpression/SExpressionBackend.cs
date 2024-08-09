ab98009be52ef6f37ebb545522fdde1a2ef4492b
dnezam <55559979+dnezam@users.noreply.github.com>
2024-08-09 08:31:47 +0200

    Change extension from sexpr to sexp

--- Source/DafnyCore/Backends/SExpression/SExpressionBackend.cs
+++ Source/DafnyCore/Backends/SExpression/SExpressionBackend.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 namespace Microsoft.Dafny.Compilers;
 public class SExpressionBackend : DafnyExecutableBackend {
   public override string TargetName => "S-expression";
-  public override string TargetExtension => "sexpr";
-  public override IReadOnlySet<string> SupportedExtensions => new HashSet<string> { ".sexpr" };
+  public override string TargetExtension => "sexp";
+  public override IReadOnlySet<string> SupportedExtensions => new HashSet<string> { ".sexp" };
   // set to true so we can see it as an option when using `dafny translate`
   public override bool IsStable => true;
   // set to false since we always want to write to disk
